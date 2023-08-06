b1c8a0ac7f07a56f9ada55c1270ded5356c5d963
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-08-06 22:16:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-pet-project-front',
+  base: '/react_node_fs_67_NMarkhotsky',
   server: {
-    open: '/your-pet-project-front',
+    open: '/react_node_fs_67_NMarkhotsky',
     port: 3000,
   },
 });
